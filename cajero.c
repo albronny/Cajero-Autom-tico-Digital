0e51b0bc0a210a4f596ead42f166e6c83adca362
Ronny Alberto Cruz Baez <albronny55@gmail.com>
2026-04-29 21:09:00 -0400

    agregar usuarios y saldos iniciales

--- cajero.c
+++ cajero.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+
+    char usuarios[3][20] = {"ronny", "angel", "julio"};
+    char claves[3][20] = {"1234", "5678", "abcd"};
+    float saldos[3] = {5000.0, 3000.0, 7000.0};
+
     printf("\nSISTEMA DE CAJERO AUTOMATICO\n\n");
+
     return 0;
 }
