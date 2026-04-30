bd6476294b216b35fa8898089a392c77ea6f84e1
Ronny Alberto Cruz Baez <albronny55@gmail.com>
2026-04-29 21:19:33 -0400

    agregar el login de usuarios

--- cajero.c
+++ cajero.c
@@ -7,7 +7,29 @@ int main() {
     char claves[3][20] = {"1234", "5678", "abcd"};
     float saldos[3] = {5000.0, 3000.0, 7000.0};
 
+    char usuario[20], contrasena[20];
+    int acceso = 0;
+    int posicionUsuario = -1;
+
     printf("\nSISTEMA DE CAJERO AUTOMATICO\n\n");
+
+    printf("Ingrese usuario: ");
+    scanf("%s", usuario);
+
+    printf("Ingrese contrasena: ");
+    scanf("%s", contrasena);
+
+    for(int i = 0; i < 3; i++) {
+        if(strcmp(usuario, usuarios[i]) == 0 && strcmp(contrasena, claves[i]) == 0) {
+            acceso = 1;
+            posicionUsuario = i;
+            break;
+        }
+    }
+
+    if(acceso == 0) {
+        printf("Credenciales incorrectas.\n");
+    }
 
     return 0;
 }
